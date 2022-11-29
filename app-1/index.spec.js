b8ba5d973067d0ec499d69b0beda7190709e5a26
ypolosov <ypolosov@gmail.com>
2022-11-29 16:01:17 +0000

    sdfsd

--- app-1/index.spec.js
+++ app-1/index.spec.js
@@ -25,8 +25,7 @@ callback = function(response) {
     }else{
       console.error(`It must be: ${expectedGreeting} instead of ${str}`);
     //   process.exit(1);
-      process.exitCode = 1;
-      throw new Error("my module xx condition failed");
+    process.on('exit', function() { process.exit(1); });
     }
   });
 }
