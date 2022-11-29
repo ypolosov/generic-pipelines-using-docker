e7219cfde76f84710f3424e9cbf02ba7c7c14b11
ypolosov <ypolosov@gmail.com>
2022-11-29 09:12:14 +0000

    sdfsdf

--- generic-pipelines/stages/04_archive.sh
+++ generic-pipelines/stages/04_archive.sh
@@ -3,6 +3,8 @@
 echo
 echo "Archiving Application"
 
+pushd "$PROJECT_DIR"
+
 registry=$(jq -r .archive.registry pipeline.json)
 account=$(jq -r .archive.account pipeline.json)
 appName=$(jq -r .archive.appName pipeline.json)
@@ -11,3 +13,5 @@ image="${registry}/${account}/${appName}:${tagName}"
 
 docker build -t "${image}" .
 docker push "${image}"
+
+popd
