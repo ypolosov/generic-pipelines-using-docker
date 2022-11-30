45b29b044cbf1d8e8a428bb316d97a3131c63232
ypolosov <ypolosov@gmail.com>
2022-11-30 08:18:50 +0000

    sdfsdf

--- app-2/index.spec.js
+++ app-2/index.spec.js
@@ -1,6 +1,6 @@
 const {greeting, startApp} = require("./index.js");
 
-const expectedGreeting = 'Hello app-2';
+const expectedGreeting = 'Hello app-3';
 if(startApp() === expectedGreeting){
     console.log(greeting);
 }else{
