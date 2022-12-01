ff77614b0848e7e8e6f9aa2444dd7af889c9737a
ypolosov <ypolosov@gmail.com>
2022-12-01 07:07:16 +0000

    sdf

--- generic-pipelines/stages/04_archive.sh
+++ generic-pipelines/stages/04_archive.sh
@@ -12,10 +12,11 @@ appName=$(jq -r .archive.appName pipeline.json)
 tagName=$(jq -r .archive.tagName pipeline.json)
 image="${registry}/${account}/${appName}:${tagName}"
 
-docker login \
-    -u "${DOCKER_USERNAME?:}" \
-    -p "${DOCKER_PASSWORD?:}" \
-    "${registry}"
+# docker login \
+#     -u "${DOCKER_USERNAME?:}" \
+#     -p "${DOCKER_PASSWORD?:}" \
+#     "${registry}"
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker build -t "${image}" .
 docker push "${image}"
 
