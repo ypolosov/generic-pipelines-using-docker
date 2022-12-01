a104bf22a066fc1b8ab76c9eab659a4d44218dc3
ypolosov <ypolosov@gmail.com>
2022-12-01 09:09:05 +0000

    sdfsdf

--- generic-pipelines/stages/04_archive.sh
+++ generic-pipelines/stages/04_archive.sh
@@ -3,8 +3,6 @@ set -e
 
 echo
 echo "Archiving Application"
-echo "${DOCKER_USERNAME?:}"
-echo "${DOCKER_PASSWORD?:}"
 
 pushd "$PROJECT_DIR"
 
@@ -14,10 +12,6 @@ appName=$(jq -r .archive.appName pipeline.json)
 tagName=$(jq -r .archive.tagName pipeline.json)
 image="${registry}/${account}/${appName}:${tagName}"
 
-# docker login \
-#     -u "${DOCKER_USERNAME?:}" \
-#     -p "${DOCKER_PASSWORD?:}" \
-#     "${registry}"
 echo "${DOCKER_PASSWORD?:}" | docker login -u "${DOCKER_USERNAME?:}" --password-stdin "${registry}"
 docker build -t "${image}" .
 docker push "${image}"
