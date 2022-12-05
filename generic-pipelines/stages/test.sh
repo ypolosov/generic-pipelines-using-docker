8f1480390b059c2c47ee386963deb344b038b00a
ypolosov <ypolosov@gmail.com>
2022-12-05 15:57:01 +0000

    sdfsdf

--- generic-pipelines/stages/test.sh
+++ generic-pipelines/stages/test.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 
 # ssh ypolosov@37.139.105.194 -vvv -p 2222 hostname -f
-ssh ypolosov@51.250.75.136 -vvv -o StrictHostKeyChecking=no -p 22 hostname -f
+ssh ypolosov@51.250.75.136 -vvv -p 22 hostname -f
