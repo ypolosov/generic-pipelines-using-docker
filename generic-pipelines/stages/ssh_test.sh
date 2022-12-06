232eb4407ac9bbf031c44235594956155eb7bb5a
ypolosov <ypolosov@gmail.com>
2022-12-06 12:27:59 +0000

    sdfsdf

---
+++ generic-pipelines/stages/ssh_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+pushd "$PROJECT_DIR"
+
+deployUser=$(jq -r .deploy.user pipeline.json)
+deployHost=$(jq -r .deploy.host pipeline.json)
+deployPort=$(jq -r .deploy.port pipeline.json)
+set -x
+
+# IMPORTANT !!! https://circleci.com/docs/github-integration/#establish-the-authenticity-of-an-ssh-host
+ssh-keyscan -p ${deployPort} ${deployHost} >> ~/.ssh/known_hosts
+ssh ${deployUser}@${deployHost} -p ${deployPort} hostname -f
--- generic-pipelines/stages/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-# IMPORTANT !!! https://circleci.com/docs/github-integration/#establish-the-authenticity-of-an-ssh-host
-ssh-keyscan -p 22 51.250.75.136 >> ~/.ssh/known_hosts
-ssh ypolosov@51.250.75.136 -p 22 hostname -f
