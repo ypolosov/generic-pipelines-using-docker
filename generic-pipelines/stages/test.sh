2a611b79f4c2f109ca61be21710ebe9b6ce2b55c
ypolosov <ypolosov@gmail.com>
2022-12-06 08:28:05 +0000

    sdsd

--- generic-pipelines/stages/test.sh
+++ generic-pipelines/stages/test.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-# ssh ypolosov@37.139.105.194 -vvv -p 2222 hostname -f
+# IMPORTANT !!! https://circleci.com/docs/github-integration/#establish-the-authenticity-of-an-ssh-host
 ssh-keyscan -p 22 51.250.75.136 >> ~/.ssh/known_hosts
 ssh ypolosov@51.250.75.136 -p 22 hostname -f
