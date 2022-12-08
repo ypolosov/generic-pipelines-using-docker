cb427e9f0da2ba3824ef5957032cdd68d5527d9a
ypolosov <ypolosov@gmail.com>
2022-12-08 10:01:29 +0000

    sdfsdf

--- agnostic-pipeline/stages/03_test.sh
+++ agnostic-pipeline/stages/03_test.sh
@@ -6,31 +6,10 @@ echo "Testing Application"
 
 pushd "$PROJECT_DIR"
 
-appType=$(jq -r .application.type pipeline.json)
-port=$(jq -r .application.port pipeline.json)
-appName=$(jq -r .application.name pipeline.json)
-testPath=$(jq -r .test.path pipeline.json)
+source '.env' 
 
 popd
 
-case "${appType}" in
-  "java")
-    mvn test
-    ;;
-  "netcore")
-    # The path to the test project must be set until
-    # https://github.com/Microsoft/vstest/issues/1129 is
-    # resolved.
-    dotnet test "${testPath}"
-    ;;
-  "node")
-    export PORT=${port}
-    export APP_NAME=${appName}
-    npm test
-    ;;
-  *)
-    echo "Unable to test application type ${appType}"
-    exit 1
-    ;;
-esac
+npm run test
+
 
