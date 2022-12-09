02051f7aaab590be29a9feb1ebb90e4947b6ab8d
ypolosov <ypolosov@gmail.com>
2022-12-09 13:54:24 +0000

    sdfsdf

--- agnostic-pipeline/stages/05_deploy.sh
+++ agnostic-pipeline/stages/05_deploy.sh
@@ -19,7 +19,7 @@ IMAGE="${ARCHIVE_REGISTRY}/${ARCHIVE_ACCOUNT}/${ARCHIVE_APP_NAME}:${ARCHIVE_TAG_
 #     chmod 644 ~/.ssh/known_hosts
 # fi
 # IMPORTANT !!! https://circleci.com/docs/github-integration/#establish-the-authenticity-of-an-ssh-host
-ssh-keyscan -p "${DEPLOY_PORT}" "${DEPLOY_HOST}" >> ~/.ssh/known_hosts
+# ssh-keyscan -p "${DEPLOY_PORT}" "${DEPLOY_HOST}" >> ~/.ssh/known_hosts
 # ssh -o StrictHostKeyChecking=no -p ${deployPort} ${deployUser}@${deployHost}
 docker -H "ssh://${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PORT}" run --rm -d --net host -e "PORT=${PORT}" -e "APP_NAME=${APP_NAME}" "${IMAGE}"
 
